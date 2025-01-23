56ba2f7f8de2ca6605746c1bbbf4130f4784fa48
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-23 12:00:01 +0530

    Delete login.js

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { loginController, registerController, getProfileController } = require('../controllers/authCtrl');
-const authRouter = express.Router();
-
-authRouter.post('/login', loginController)
-authRouter.post('/register', registerController)
-authRouter.get('/get-profile', getProfileController)
-
-module.exports = authRouter;
