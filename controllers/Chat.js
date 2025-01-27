8a1320d9fae1b419bc84dc9759ee51a2d8787190
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-27 18:34:06 +0530

    Update Chat.js

--- controllers/Chat.js
+++ controllers/Chat.js
@@ -1,27 +1,65 @@
 
-const CHAT = require('../modal/realTimeChat')
+const CHAT = require('../modal/realTimeChat');
+const USER = require('../modal/user');
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config();
+
 
 const CreateNewMessage = async (req, res) => {
-  const { senderId, message } = req.body;
+  const { senderId, message, receiverId } = req.body;
   if (!message) {
-    return res.status(400).json({ error: 'Please provide a message.' });
+    return res.status(400).json({ message: 'Please provide a message.' });
+  }
+
+  if (!receiverId || !senderId) {
+    return res.status(400).json({ message: 'Please provide receiver id and sender id.' });
   }
   else {
     const newMessage = new CHAT({
       senderId,
-      message
+      message,
+      receiverId
     });
     await newMessage.save();
-    res.status(200).json({message:'message send successfully!',status_Code:200});
+    res.status(200).json({ message: 'message send successfully!', status_Code: 200 });
   }
 
 }
 
-const getMessages=async(req,res)=>{
-
-  const messages=await CHAT.find()
-  res.status(200).json({messages,status_code:200});
+const getMessages = async (req, res) => {
+  const messages = await CHAT.find()
+  res.status(200).json({ messages, status_code: 200 });
 
 
 }
-module.exports={CreateNewMessage,getMessages}
+
+
+
+const getUserMessage = async (req, res) => {
+  const { id } = req.params;
+  const userId = parseInt(id);
+  const token = req.headers['authorization']?.replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided or token is malformed.' });
+  }
+  const decode = jwt.verify(token, process.env.JWT_SECRET);
+  const loginUserId = decode.user.id
+  if (!loginUserId) {
+    return res.status(401).json({ error: 'Invalid token.' });
+  }
+  const messages = await CHAT.find({
+    $or: [
+      { senderId: loginUserId, receiverId: userId },
+      { senderId: userId, receiverId: loginUserId },
+    ]
+  })
+
+
+  console.log(messages);
+  res.status(200).json({ messages, status_code: 200 });
+
+
+}
+
+module.exports = { CreateNewMessage, getMessages, getUserMessage }
