ac1fe852be5284d54e452e0892fbb8d6804b422d
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-27 19:31:30 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,19 +6,16 @@ dotenv.config();
 
 const CreateNewUser = async (req, res) => {
   const { name, email, password } = req.body
-
   if (!email || !password) {
     return res.status(400).json({ message: 'required email or password', status_code: 400 })
   }
   const validateEmail = (email) => {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email); // Returns tr ue if email matches the regex, otherwise false.
+    return emailRegex.test(email);
   }
-
   if (!validateEmail(email)) {
     return res.status(400).json({ message: 'Invalid email format', status_code: 400 })
   }
-
   const AlredyExist = await USER.findOne({ email })
   if (AlredyExist) {
     return res.status(400).json({ message: 'User already exist', status_code: 400 })
@@ -36,21 +33,17 @@ const LoginUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await USER.findOne({ email, password })
   const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '10y' });
-
   if (!email, !password) {
     return res.status(400).json({ message: 'required email or password', status_code: 400 })
   }
   if (!user) {
     return res.status(401).json({ message: 'Invalid email or password', status_code: 401 })
   }
-  res.status(200).json({ message: "success", status_code: 200, data: user, token: token })
-  console.log(token);
-
+  res.status(200).json({ message: "User logged in successfully", status_code: 200, data: user, token: token })
 }
 
 const getUserList = async (req, res) => {
   const token = req.headers['authorization'].replace('Bearer ', '')
-
   if (!token) {
     return res.status(401).json({ error: 'No token provided.' });
   }
@@ -59,24 +52,22 @@ const getUserList = async (req, res) => {
   if (!Id) {
     return res.status(401).json({ error: 'Invalid token.' });
   }
-
   const users = await USER.find({ id: { $ne: Id } })
   res.status(200).json({ users, status_code: 200, message: 'find user Successfully!' });
 
 }
+
 const getUser = async (req, res) => {
   const id = parseInt(req.params.id)
   const user = await USER.findOne({ id: id })
-
   if (!Number.isInteger(id) || id <= 0) {
     return res.status(400).json({ message: 'Invalid user id', status_code: 400 })
   }
-
   if (!user) {
     return res.status(404).json({ message: 'User not found', status_code: 404 })
   }
   res.status(200).json({ user, status_code: 200 });
-
-
 }
+
+
 module.exports = { CreateNewUser, LoginUser, getUserList, getUser }
