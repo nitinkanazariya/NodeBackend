39eea7fb1ff8464fa3784ba1b33f14e505d3e5ca
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-27 18:34:08 +0530

    Update realTimeChat.js

--- modal/realTimeChat.js
+++ modal/realTimeChat.js
@@ -2,26 +2,32 @@ const mongoose = require('mongoose');
 
 // Define the schema for the chat messages
 const chatSchema = new mongoose.Schema({
-  senderId: { 
-    type: String, 
-    required: true, 
+  senderId: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  receiverId: {
+    type: String,
+    required: true,
     trim: true
   },
   message: {
-    type: String, 
-    required: true, 
+    type: String,
+    required: true,
     trim: true
   },
   timestamp: {
     type: Date,
     default: Date.now  // Automatically set the timestamp when a message is created
   },
-  
-  
+
+
 },
-{versionKey:false});
+  { versionKey: false });
 
 // Create the model based on the schema
-const Chat = mongoose.model('Chat', chatSchema,'Chat');
+const Chat = mongoose.model('Chat', chatSchema, 'Chat');
 
 module.exports = Chat;
